cd1015a520bbb817690c4b8dc3178722d91f2721
ana ketllyn <105050863+anaaak@users.noreply.github.com>
2024-10-25 11:04:13 +0000

    k

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,5 +4,6 @@ async function visualizarInformacoesGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
     console.log(dados);
-    
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add('graficos-container__texto');
 }
